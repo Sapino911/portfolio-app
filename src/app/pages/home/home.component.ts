feb38c905f5e67c5c7f11002185a0efe1d3a3576
Sapino911 <letsapamasego@gmail.com>
2026-06-18 01:43:23 +0200

    feat: add Hero component to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { HeroComponent } from "../../components/hero/hero.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
