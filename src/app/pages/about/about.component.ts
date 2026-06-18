8ab16478d82c51ff1b3fe8b6354dd94e09877e7a
Sapino911 <letsapamasego@gmail.com>
2026-06-18 04:32:17 +0200

    feat: implement About section with dynamic highlights and integrate into home page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+
+ 
+
+  protected readonly highlights = [
+    { value: '6+', title: 'Years Experience', textColor: 'text-pink-500' },
+    { value: '10+', title: 'Projects Built', textColor: 'text-blue-500' },
+    { value: '5+', title: 'Technologies', textColor: 'text-green-500' },
+    { value: '100%', title: 'Problem Solver', textColor: 'text-yellow-500' }
+  ];
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { HeroComponent } from "../../components/hero/hero.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
