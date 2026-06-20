604ec5f7d01fb6069eac3a2341ab8dd066e3a8d4
Sapino911 <letsapamasego@gmail.com>
2026-06-20 05:19:54 +0200

    feat: implement Contact section with contact details and message button; update Home component to include Contact section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,12 @@ import { SkillCardComponent } from "../../components/skill-card/skill-card.compo
 import { ProjectCardComponent } from "../../components/project-card/project-card.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { ExperienceComponent } from "../experience/experience.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillCardComponent, ProjectCardComponent, ProjectsComponent, ExperienceComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillCardComponent, ProjectCardComponent, ProjectsComponent, ExperienceComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
